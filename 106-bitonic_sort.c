2c1061cbbf1da8a156a92920e04a08a9ce79ed8d
muriukialex-n <muriukialex.n@gmail.com>
2023-09-24 12:03:56 -0700

    trial

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,6 +1,5 @@
 #include "sort.h"
 #include <stdio.h>
-
 /**
  * swap - Swap two elements in the array.
  * @arr: The array.
@@ -9,11 +8,11 @@
  */
 void swap(int *arr, int i, int j)
 {
-    int temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-}
+	int temp = arr[i];
 
+	arr[i] = arr[j];
+	arr[j] = temp;
+}
 /**
  * bitonic_merge - Recursively merge the bitonic sequences.
  * @arr: The array.
@@ -23,26 +22,23 @@ void swap(int *arr, int i, int j)
  */
 void bitonic_merge(int *arr, int low, int nelemnt, int order)
 {
-    if (nelemnt > 1)
-    {
-        int mid = nelemnt / 2;
+	if (nelemnt > 1)
+	{
+		int mid = nelemnt / 2;
 
-        printf("Merging [%i/%i] (%s):\n", nelemnt, nelemnt, order ? "UP" : "DOWN");
-        print_array(arr + low, nelemnt);
-
-        for (int i = low; i < low + mid; i++)
-        {
-            if ((arr[i] > arr[i + mid]) == order)
-            {
-                swap(arr, i, i + mid);
-            }
-        }
-
-        bitonic_merge(arr, low, mid, order);
-        bitonic_merge(arr, low + mid, mid, order);
-    }
+		printf("Merging [%i/%i] (%s):\n", nelemnt, nelemnt, order ? "UP" : "DOWN");
+		print_array(arr + low, nelemnt);
+		for (int i = low; i < low + mid; i++)
+		{
+			if ((arr[i] > arr[i + mid]) == order)
+			{
+				swap(arr, i, i + mid);
+			}
+		}
+		bitonic_merge(arr, low, mid, order);
+		bitonic_merge(arr, low + mid, mid, order);
+	}
 }
-
 /**
  * bitonic_sort_recursive - Recursive function for Bitonic Sort.
  * @arr: The array.
@@ -52,20 +48,17 @@ void bitonic_merge(int *arr, int low, int nelemnt, int order)
  */
 void bitonic_sort_recursive(int *arr, int low, int nelemnt, int order)
 {
-    if (nelemnt > 1)
-    {
-        int mid = nelemnt / 2;
+	if (nelemnt > 1)
+	{
+		int mid = nelemnt / 2;
 
-        bitonic_sort_recursive(arr, low, mid, 1);
-        bitonic_sort_recursive(arr, low + mid, mid, 0);
-
-        bitonic_merge(arr, low, nelemnt, order);
-
-        printf("Result [%i/%i] (%s):\n", nelemnt, nelemnt, order ? "UP" : "DOWN");
-        print_array(arr + low, nelemnt);
-    }
+		bitonic_sort_recursive(arr, low, mid, 1);
+		bitonic_sort_recursive(arr, low + mid, mid, 0);
+		bitonic_merge(arr, low, nelemnt, order);
+		printf("Result [%i/%i] (%s):\n", nelemnt, nelemnt, order ? "UP" : "DOWN");
+		print_array(arr + low, nelemnt);
+	}
 }
-
 /**
  * bitonic_sort - Sorts an array of integers using Bitonic Sort.
  * @array: The array.
@@ -73,10 +66,9 @@ void bitonic_sort_recursive(int *arr, int low, int nelemnt, int order)
  */
 void bitonic_sort(int *array, size_t size)
 {
-    if (!array || size < 2)
-    {
-        return;
-    }
-
-    bitonic_sort_recursive(array, 0, size, 1);
+	if (!array || size < 2)
+	{
+		return;
+	}
+	bitonic_sort_recursive(array, 0, size, 1);
 }
